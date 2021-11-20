4e66065017b204ea74413393501bda090668e079
Holivane <holivane@gmail.com>
2021-11-20 15:08:35 -0300

    incluindo rotas de alterações e delete

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -37,7 +37,7 @@ class Atendimento {
                 if(erro){
                     res.status(400).json(erro)
                 } else {
-                    res.status(201).json(resultados)
+                    res.status(201).json(atendimento)
                 } 
             })
         }
@@ -66,6 +66,35 @@ class Atendimento {
                 res.status(400).json(erro)
             } else {
                 res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    altera(id, valores, res) {
+        if(valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        }
+        const sql = `UPDATE Atendimentos SET? WHERE id=?`
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({...valores,id})
+            }
+        })
+    }
+
+    
+    deleta(id, res) {
+        const sql = `DELETE FROM Atendimentos WHERE id=?`
+
+        conexao.query(sql, id, (erro, resultados) => {
+            
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
             }
         })
     }
