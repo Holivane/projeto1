875601015774e2f1086d0159cbbf209e29ba3916
Holivane <holivane@gmail.com>
2021-11-21 12:00:42 -0300

    criando arquivos com buffer e streams

---
+++ arquivos/upLoadArquivos.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+
+fs.createReadStream('./assets/gato.jpg')
+    .pipe(fs.createWriteStream('./assets/gato-stream.jpg'))
+    .on('finish', () => console.log('Imagem foi escrita com sucesso'))
--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -8,4 +8,4 @@ const conexao = mysql.createConnection({
     database: 'agenda-pet'
 })
 
-module.exports = conexao
+module.exports = conexao 
