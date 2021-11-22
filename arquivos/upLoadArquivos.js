89179968c4d79fea09713b023b4873a521903556
Holivane <holivane@gmail.com>
2021-11-22 17:04:58 -0300

    Validação de imagem nas extensões jpg, png e jpeg

--- arquivos/upLoadArquivos.js
+++ arquivos/upLoadArquivos.js
@@ -1,9 +1,21 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
 
-    fs.createReadStream(caminho)
-        .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish', () => callbackImagemCriada(novoCaminho))
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+
+    if (tipoEhValido === -1) {
+        console.log('Erro!! Tipo inválido')
+    } else {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(novoCaminho))
+    }
 }
+
+
