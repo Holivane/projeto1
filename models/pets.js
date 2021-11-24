04c8567c690049eaf482b8af735fe7eb81443ded
Holivane <holivane@gmail.com>
2021-11-24 15:17:54 -0300

    criação de mensagem de erro para usuário

--- models/pets.js
+++ models/pets.js
@@ -4,17 +4,24 @@ class Pet {
     adiciona(pet, res) {
         const query = 'INSERT INTO Pets SET ?'
 
-        upLoadArquivo(pet.imagem, pet.nome, novoCaminho => {
-            const novoPet = { nome: pet.nome, imagem: novoCaminho }
+        upLoadArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+
+            if(erro){
+                res.status(400).json(erro)
+
+            }else{
+                const novoPet = { nome: pet.nome, imagem: novoCaminho }
             
-            conexao.query(query, novoPet, erro => {
-                if (erro) {
-                    console.log(erro)
-                    res.status(400).json(erro)
-                } else {
-                    res.status(200).json(novoPet)
-                }
-            })
+                conexao.query(query, novoPet, erro => {
+                    if (erro) {
+                        console.log(erro)
+                        res.status(400).json(erro)
+                    } else {
+                        res.status(200).json(novoPet)
+                    }
+                })
+            }
+            
         })
          
     }
