51a9fb4a89212518f7fc775d6176a8353eb7ec99
Holivane Holanda <37214904+Holivane@users.noreply.github.com>
2021-12-05 19:49:51 -0300

    Atualização de senha

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -4,7 +4,7 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password:  'programadora2022',
+    password:  '1234',
     database: 'agenda-pet'
 })
 
